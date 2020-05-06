13c72abedd98d2b1276264c9c6c108de03aca7a8
Patrick Emond <emondpd@mcmaster.ca>
2020-05-06 13:58:26 -0400

    Updating script to facilitate DCS-by-phone
    The views have been changed so that one view contains all recordings
    (Thanks Dean!)

--- download_recording_by_uid_f2.php
+++ download_recording_by_uid_f2.php
@@ -10,8 +10,7 @@
  */ 
 
 define( 'RECORDING_PATH', '/data/comprehensive/f2' );
-define( 'OPAL_HOME_VIEW', 'recordings-home-f2' );
-define( 'OPAL_SITE_VIEW', 'recordings-site-all-f2' );
+define( 'OPAL_RECORDINGS_VIEW', 'recordings-f2' );
 define( 'SCRIPT_PATH', '/home/patrick/files/scripts/get_cog_recordings' );
 include SCRIPT_PATH.'/common.php';
 
@@ -26,7 +25,7 @@ else
 }
 
 out( 'getting list of recordings for '.$uid );
-$result = send_to_view( OPAL_SITE_VIEW, sprintf( 'valueSet/%s', $uid ) );
+$result = send_to_view( OPAL_RECORDINGS_VIEW, sprintf( 'valueSet/%s', $uid ) );
 if( is_object( $result ) && property_exists( $result, 'valueSets' ) )
 {
   foreach( $result->valueSets as $participant )
@@ -34,18 +33,8 @@ if( is_object( $result ) && property_exists( $result, 'valueSets' ) )
     $uid = $participant->identifier;
     out( sprintf( 'downloading recordings for "%s"', $uid ) );
     
-    // get the site files (already included in the list
     foreach( $participant->values as $recording )
       if( is_object( $recording ) && property_exists( $recording, 'link' ) ) download_file( $recording->link, RECORDING_PATH );
-
-    // get the home files (not included in the list)
-    $result = send_to_view( OPAL_HOME_VIEW, sprintf( 'valueSet/%s', $uid ) );
-    if( is_object( $result ) && property_exists( $result, 'valueSets' ) )
-    {
-      $obj = current( $result->valueSets );
-      foreach( $obj->values as $recording )
-        if( is_object( $recording ) && property_exists( $recording, 'link' ) ) download_file( $recording->link, RECORDING_PATH );
-    }
   }
 }
 else out( 'No data found' );
--- download_recordings_f2.php
+++ download_recordings_f2.php
@@ -8,9 +8,8 @@
  */
 
 define( 'RECORDING_PATH', '/data/comprehensive/f2' );
-define( 'OPAL_CHECK_VIEW', 'recordings-check-f2' );
-define( 'OPAL_HOME_VIEW', 'recordings-home-f2' );
-define( 'OPAL_SITE_VIEW', 'recordings-site-all-f2' );
+define( 'OPAL_CHECK_VIEW', 'check-f2' );
+define( 'OPAL_RECORDINGS_VIEW', 'recordings-f2' );
 define( 'CHUNK_SIZE', 1000 );
 define( 'MAX_OFFSET', 60000 );
 define( 'SCRIPT_PATH', '/home/patrick/files/scripts/get_cog_recordings' );
@@ -21,12 +20,12 @@ out( 'checking for new recordings' );
 for( $offset = 0; $offset < MAX_OFFSET; $offset += CHUNK_SIZE )
 {
   // get the check data for this offset
-  $result = send_to_view( OPAL_CHECK_VIEW, sprintf( 'valueSets?offset=%d&limit=%d', $offset, CHUNK_SIZE ) );
+  $check_result = send_to_view( OPAL_CHECK_VIEW, sprintf( 'valueSets?offset=%d&limit=%d', $offset, CHUNK_SIZE ) );
 
   // stop processing if there is no more data left (means we have passed the maximum)
-  if( !is_object( $result ) || !property_exists( $result, 'valueSets' ) ) break;
+  if( !is_object( $check_result ) || !property_exists( $check_result, 'valueSets' ) ) break;
 
-  foreach( $result->valueSets as $obj )
+  foreach( $check_result->valueSets as $obj )
   {
     $uid = $obj->identifier;
     $var = current( $obj->values );
@@ -36,23 +35,14 @@ for( $offset = 0; $offset < MAX_OFFSET; $offset += CHUNK_SIZE )
     {
       out( sprintf( 'Downloading recordings for %s', $uid ) );
 
-      $sub_result = send_to_view( OPAL_SITE_VIEW, sprintf( 'valueSet/%s', $uid ) );
-      if( is_object( $sub_result ) && property_exists( $sub_result, 'valueSets' ) )
+      $recordings_result = send_to_view( OPAL_RECORDINGS_VIEW, sprintf( 'valueSet/%s', $uid ) );
+      if( is_object( $recordings_result ) && property_exists( $recordings_result, 'valueSets' ) )
       {
-        foreach( $sub_result->valueSets as $participant )
+        foreach( $recordings_result->valueSets as $participant )
         {
-          // get the site files (already included in the list
+          // get the recording files
           foreach( $participant->values as $recording )
             if( is_object( $recording ) && property_exists( $recording, 'link' ) ) download_file( $recording->link, RECORDING_PATH );
-
-          // get the home files (not included in the list)
-          $sub_result2 = send_to_view( OPAL_HOME_VIEW, sprintf( 'valueSet/%s', $uid ) );
-          if( is_object( $sub_result2 ) && property_exists( $sub_result2, 'valueSets' ) )
-          {
-            $obj = current( $sub_result2->valueSets );
-            foreach( $obj->values as $recording )
-              if( is_object( $recording ) && property_exists( $recording, 'link' ) ) download_file( $recording->link, RECORDING_PATH );
-          }
         }
       }
       else out( 'No data found' );
